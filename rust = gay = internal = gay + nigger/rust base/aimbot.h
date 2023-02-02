69a31f458f07c8b3ea6b637f57f0d8d38299cd3c
Engineer Monkey <103351450+Asmodeus-was-taken@users.noreply.github.com>
2023-02-01 20:45:15 -0700

    Delete rust = gay = internal = gay + nigger directory

--- rust = gay = internal = gay + nigger/rust base/aimbot.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "includes.hpp"
-#include "game/classes.hpp"
-
-namespace aimbot {
-	rust::BasePlayer* getBestPlayerByFov();
-	int getClosestBoneToCrosshair(rust::BasePlayer* player);
-	void run(bool forceHead);
-}
