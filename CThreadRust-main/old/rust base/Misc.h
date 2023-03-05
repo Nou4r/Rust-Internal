3389920449c27857af479baa8868aa720500e64f
GenesiusX <77931801+GenesiusX@users.noreply.github.com>
2023-03-04 23:26:27 -0500

    Delete CThreadRust-main/old directory

--- CThreadRust-main/old/rust base/Misc.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "includes.hpp"
-#include "game/classes.hpp"
-
-namespace Misc {
-
-}
--- CThreadRust-main/old/rust base/sky.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "includes.hpp"
-#include "game/classes.hpp"
-
-namespace sky {
-	void sky(rust::TOD_Sky SKY);
-}
